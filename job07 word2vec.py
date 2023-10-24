56bbab4dc91610c4d187daaa592f56b5de3e096e
wjgk0822 <35884296+wjgk0822@users.noreply.github.com>
2023-10-24 16:45:53 +0900

    6 commit
    6 commit

--- job07 word2vec.py
+++ job07 word2vec.py
@@ -21,7 +21,11 @@ print(tokens[0])
 
 embedding_model=Word2Vec(tokens,vector_size=100,window=4,min_count=20,workers=10,epochs=100,sg=1)
 
+embedding_model.save('D:/AI_exam/intel_second_crawling/models/word2vec_movie_review.model')
 
+print(list(embedding_model.wv.index_to_key))
+
+print(len(embedding_model.wv.index_to_key))
 
 
 
